0c3bec8ddc73b9548e3d6bf98a66978933d7c0c5
Ridwan <aladebaruwa@gmail.com>
2023-06-27 11:54:07 +0100

    add

--- index.js
+++ index.js
@@ -30,3 +30,19 @@ var num = 5;
 
 // Store string in the variable num
 num = "GeeksforGeeks";
+
+// calling x after definition
+var x = 5;
+document.write(x, "\n");
+ 
+// calling y after definition
+let y = 10;
+document.write(y, "\n");
+ 
+// calling var z before definition will return undefined
+document.write(z, "\n");
+var z = 2;
+ 
+// calling let a before definition will give error
+document.write(a);
+let a = 3;
