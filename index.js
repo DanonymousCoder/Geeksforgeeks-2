e204d0ce5260441ab1a8b9cb54ef647f93fda7f0
Ridwan <aladebaruwa@gmail.com>
2023-06-27 11:57:23 +0100

    add

--- index.js
+++ index.js
@@ -46,3 +46,36 @@ var z = 2;
 // calling let a before definition will give error
 document.write(a);
 let a = 3;
+
+<h1 id="var" style="color:black;">GeeksforGeeks</h1>
+<h1 id="let" style="color:black;">GeeksforGeeks</h1>
+<button id="btn" onclick="colour()">Start</button>
+<!-- executing function on button click -->
+
+<script type="text/javascript">
+	function colour() {
+	
+		setInterval(function() {
+	
+			if (document.getElementById('var').style.color == 'black')
+				var col1 = 'blue';
+			else
+				col1 = 'black';
+	
+			// setting value of color 1 through var
+			if (document.getElementById('let').style.color == 'black') {
+				let col2 = 'red';
+			} else {
+				col2 = 'black';
+			}
+	
+			// setting value of color 2 through let
+			document.getElementById('var').style.color = col1;
+	
+			document.getElementById('let').style.color = col2;
+	
+			// changing color of h1 in html
+		}, 500);
+	
+	}
+
