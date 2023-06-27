a96c5d73d39946ae302be146403e230043c2d146
Ridwan <aladebaruwa@gmail.com>
2023-06-27 12:17:51 +0100

    add

--- index.js
+++ index.js
@@ -73,3 +73,22 @@ let a = 3;
 	
 	}
 
+	let petName = 'Rocky' // Global variable
+	myFunction()
+	
+	function myFunction() {
+		fruit = 'apple'; // Considered global
+		console.log(typeof petName +
+			'- ' +
+			'My pet name is ' +
+			petName)
+	}
+	
+	console.log(
+		typeof petName +
+		'- ' +
+		'My pet name is ' +
+		petName +
+		'Fruit name is ' +
+		fruit)
+	
